ee5b3890509f013d608c46039df231a0c94ddd68
Lucas Gao <lucas2005gao@gmail.com>
2020-03-07 17:02:55 +1300

    auto load helpers

---
+++ application/controllers/EnrollmentForm.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class EnrollmentForm extends CI_Controller {
+
+	function __construct() {
+		parent::__construct();
+		// $this->load->helper();
+		// $this->load->model();
+	}
+
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	
+}
