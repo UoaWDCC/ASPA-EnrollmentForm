405a1e97db3f1ca0bfc4ea68951dd5d6229a149d
Lucas Gao <ygao979@UoA.auckland.ac.nz>
2020-03-13 16:00:59 +1300

    assets/js url fix

--- application/views/EnrollmentForm.php
+++ application/views/EnrollmentForm.php
@@ -109,7 +109,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     </div>
   </div>
   <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-  <script src="assets/webflow.js" type="text/javascript"></script>
+  <script src="assets/js/webflow.js" type="text/javascript"></script>
   <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 </body>
 </html>
