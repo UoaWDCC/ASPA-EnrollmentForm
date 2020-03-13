6bbc4051b65b645a2531a580e92688343c4c1563
Lucas Gao <ygao979@UoA.auckland.ac.nz>
2020-03-13 14:59:44 +1300

    added exports from webflow

--- application/views/EnrollmentForm.php
+++ application/views/EnrollmentForm.php
@@ -2,35 +2,114 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
-<html lang="en">
-
+<html data-wf-page="5e66056a415f1ee3724d7631" data-wf-site="5e66056a415f1ee8ad4d7630">
 <head>
-	<meta charset="utf-8">
-	<title>ASPA Event registration form</title>
-	<meta content="The online enrollment for Auckland Student Pool Association's first event">
+  <meta charset="utf-8">
+  <title>ASPA</title>
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/webflow.css" rel="stylesheet" type="text/css">
+  <link href="css/aspa-ddafc5.webflow.css" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+  <script type="text/javascript">WebFont.load({  google: {    families: ["Droid Sans:400,700"]  }});</script>
+  <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+  <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link href="images/webclip.png" rel="apple-touch-icon">
 </head>
-
 <body>
-<style>
-	button {
-		display: block;
-	}
-</style>
-
-<button class="btn" id="button-Description">ok I'v read the description</button>
-<button class="btn" id="button-NameEmailPage">ok check my name and email</button>
-<br>
-<button class="btn" id="button-cash">cash</button>
-<button class="btn" id="description-Alipay">bank transfer</button>
-<br>
-<button class="btn" id="button-Wechat">WechatPay</button>
-<button class="btn" id="button-alipay">Alipay</button>
-<button class="btn" id="description-CreditCard">Credit Card</button>
-<button class="btn" id="button-Wechat">PoLiPay</button>
-<br>
-<button class="btn" id="send-email" href="/EnrollmentForm/send_email">Send Email</button>
-
-
+  <div class="w-form">
+    <form id="email-form" name="email-form" data-name="Email Form">
+      <div class="section">
+        <div class="content-block">
+          <div class="div-block page1">
+            <div class="div-placeholder"></div>
+            <div class="div-main-page-right">
+              <h1 class="heading">Welcome</h1>
+              <h1 class="heading-2">ASPA 2020 Welcome Event</h1>
+              <div class="div-main-page"><a data-w-id="dfbe5add-65ea-95a7-6380-331c1db905e2" href="#" class="button w-button">Register</a>
+                <p class="paragraph-pressenter">press <strong class="bold-enter">Enter ↵</strong></p>
+              </div>
+            </div>
+          </div>
+          <div class="div-block page2">
+            <div class="div-main-text">
+              <p class="p">Bring your cue skills, friends, and pool rivals to ASPA&#x27;s first event for 2019!<br><br>Limited availability so sign up to confirm your spot!<br>‍<br>(this form will close upon reaching capacity so if you&#x27;re reading this its not too late :D)<br>‍<br>When: Thursday 14th March 6:30pm - 8:00pm<br>‍<br>Where: Orange Pool Club (Corner of Liverpool St and City Rd)<br>‍<br>Make sure you are.<br>‍</p>
+            </div>
+            <div class="div-okbtn">
+              <div class="div-placeholder ok-btn-spaceholder"></div><a data-w-id="9bf4b69b-3e76-a6f8-a909-2eb2c799c775" href="#" class="button btn-ok w-button">OK ✓</a>
+              <p class="p-ok-btn-confirm p">Press<strong> Enter ↵</strong></p>
+            </div>
+          </div>
+          <div class="div-block page3">
+            <div class="div-name-email">
+              <div class="div div-name">
+                <p class="p p-firstname">Your first and last name:</p><input type="text" class="entry w-input" maxlength="256" name="field" data-name="Field" placeholder="Enter your first name" id="field" required=""></div>
+              <div class="div div-email">
+                <p class="p p-lastname">Email address: </p><input type="text" class="entry entry-lastname w-input" maxlength="256" name="field-2" data-name="Field 2" placeholder="Enter your last name" id="field-2" required="">
+                <div class="div-block-4"><img src="images/green-tick.png" width="40" alt="" class="image-3"><img src="images/exclamation.png" width="30" srcset="images/exclamation-p-500.png 500w, images/exclamation.png 777w" sizes="30px" alt="" class="image"><img src="images/Spinner-1s-200px-2.svg" width="50" alt="" class="image-2"></div>
+              </div>
+              <div class="div div-errormsg">
+                <p class="p p-errormessage">Unrecognized email, please use the email you signed up to ASPA with.<br></p>
+                <p class="p p-errormessage">If you are not a member yet, please register first.</p>
+              </div>
+            </div>
+            <div class="div-okbtn">
+              <div class="div-placeholder ok-btn-spaceholder"></div><a data-w-id="ca7b1509-24a9-effc-9e1e-24dfb568363f" href="#" class="button btn-ok w-button">OK ✓</a>
+              <p class="p-ok-btn-confirm p">Press<strong> Enter ↵</strong></p>
+            </div>
+          </div>
+          <div class="div-block page4">
+            <div class="div">
+              <p class="p p-payment">Choose your method of payment: </p>
+            </div>
+            <div class="div-grid-wrapper">
+              <div class="w-layout-grid grid grid-offline">
+                <div id="w-node-2a7cf43bcd10-724d7631" class="div-block-5">
+                  <p class="p">Pay on the day:</p>
+                </div>
+                <div id="w-node-2922c9864b4c-724d7631" class="div-offlinepay"><a href="#" class="btn-offline w-button"></a>
+                  <p class="p p-paymentbutton">Cash</p>
+                </div>
+                <div id="w-node-b3c547ea4b4f-724d7631" class="div-offlinepay"><a href="#" class="btn-offline btn-banktransfer w-button"></a>
+                  <p class="p p-paymentbutton">Bank Transfer</p>
+                </div>
+              </div>
+              <div class="w-layout-grid grid grid-online">
+                <div id="w-node-ccd45ade939b-724d7631" class="div-creditcard"><a href="#" class="btn-online w-button"></a>
+                  <p class="p p-paymentbutton">Credit/Debit Card</p>
+                </div>
+                <div id="w-node-79804cf717f6-724d7631" class="div-wechatpay"><a href="#" class="btn-online btn-wechatpay w-button"></a>
+                  <p class="p p-paymentbutton">Wechat Pay</p>
+                </div>
+                <div id="w-node-1287f971e519-724d7631" class="div-polipay"><a href="#" class="btn-online btn-polipay w-button"></a>
+                  <p class="p p-paymentbutton">PoLi Pay</p>
+                </div>
+                <div id="w-node-b10b15011cf4-724d7631" class="div-alipay"><a href="#" class="btn-online btn-alipay w-button"></a>
+                  <p class="p p-paymentbutton">AliPay</p>
+                </div>
+                <div id="w-node-49a376fead40-724d7631" class="div-block-6">
+                  <p class="p">Pay online to avoid the queue:</p>
+                </div>
+              </div>
+            </div>
+            <div class="div-okbtn div-submit">
+              <div class="div-placeholder ok-btn-spaceholder btn-submit"></div><a data-w-id="e789d92c-5d47-387a-12e6-10a0e389e4ba" href="#" class="button btn-submit w-button">SUBMIT</a>
+              <p class="p-ok-btn-confirm p">Press<strong> Enter ↵</strong></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
+    <div class="w-form-done">
+      <div>Thank you! Your submission has been received!</div>
+    </div>
+    <div class="w-form-fail">
+      <div>Oops! Something went wrong while submitting the form.</div>
+    </div>
+  </div>
+  <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+  <script src="js/webflow.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 </body>
-
 </html>
