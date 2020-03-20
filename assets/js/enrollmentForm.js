d6d4640a17a6a3576e5d41aecda56153cb39d6b8
MartinTiangco <martintiangco@gmail.com>
2020-03-20 22:48:22 +1300

    Organised code into logical sections (e.g. Page Navigation section), added more comments

--- assets/js/enrollmentForm.js
+++ assets/js/enrollmentForm.js
@@ -1,93 +1,61 @@
+// ==========================================
+//       Getting the document elements
+// ==========================================
+
 // get the pages
 let page1 = document.getElementById("div-page1");
 let page2 = document.getElementById("div-page2");
 let page3 = document.getElementById("div-page3");
 let page4 = document.getElementById("div-page4");
 
-let buttonReg = document.getElementById("register"); // get register button from page 1
-let ok2 = document.getElementById("ok2"); // get ok button from page 2
-let ok3 = document.getElementById("ok3"); // get ok button from page 3
-// get the submit / proceed payment buttons from page 4
-let submit = document.getElementById("div-submit");
-let proceedPayment = document.getElementById("div-proceedpayment");
+// get the buttons
+let buttonReg = document.getElementById("register"); // register button from page 1
+let ok2 = document.getElementById("ok2"); // ok button from page 2
+let ok3 = document.getElementById("ok3"); // ok button from page 3
+let back3 = document.getElementById("div-back-page3"); // back button from page 3
+let submit = document.getElementById("div-submit"); // submit button from page 4
+let proceedPayment = document.getElementById("div-proceedpayment"); // proceed payment button from page 4
+let back4 = document.getElementById("div-back-page4"); // back button from page 4
 
-// get the back buttons from page 3 and page 4
-let back3 = document.getElementById("div-back-page3");
-let back4 = document.getElementById("div-back-page4");
-
-// get name and email inputs
+// get name and email inputs on page 3
 let inputName = document.getElementById("field");
 let inputEmail = document.getElementById("field-2");
 
-// Enter key (keycode 13) triggers the click event of the appropriate buttons to go to next page.
-window.addEventListener("keypress", function(e) {
-	if (e.keyCode === 13) {
-		console.log("Enter is pressed");
-		switch (findActivePage()) {
-			case 1:
-				buttonReg.click(); //buttonReg does not need an onclick function declaration because it is handled by Webflow
-				break;
-			case 2:
-				ok2.click(); //ok2 does not need an onclick function declaration because it is handled by Webflow
-				break;
-			case 3:
-				ok3.click();
-				break;
-			case 4:
-				if (isActive(submit)) submit.click();
-				else if (isActive(proceedPayment)) proceedPayment.click();
-				break;
-		}
-	}
-});
+// get email validation icons/messages on page 3
+let tickEmail = document.getElementById("tick-email");
+let exclamationEmail = document.getElementById("exclamation-email");
+let loading = document.getElementById("loading");
+let errorMsgs = document.getElementsByClassName("div-errormsg");
 
-// initial state
+// get the payment button types on page 4
+let payCash = document.getElementById("btn-cash");
+let payTransfer = document.getElementById("btn-banktransfer");
+let payWeChat = document.getElementById("btn-wechat");
+let payAli = document.getElementById("btn-alipay");
+let payCard = document.getElementById("btn-creditcard");
+let payPoli = document.getElementById("btn-poliay");
+
+// ==========================================
+//        Setup of Initial state
+// ==========================================
+
 page2.style.display = "none";
 page3.style.display = "none";
 page4.style.display = "none";
 
-// name/email page (page 3) OK button onclick name and email validation
-ok3.onclick = function() {
-	if (inputName.value.length === 0) {
-		console.log("Name has no value", inputName);
-		return;
-	}
-	if (inputEmail.value.length === 0) {
-		console.log("Email has no value", inputEmail);
-		tickEmail.style.display = "none";
-		exclamationEmail.style.display = "block";
-		errorMsgs[0].style.display = "block";
-		return;
-	}
-	if (!validateEmail(inputEmail.value)) {
-		console.log("Email is not valid!", inputEmail);
-		tickEmail.style.display = "none";
-		exclamationEmail.style.display = "block";
-		errorMsgs[0].style.display = "block";
-		return;
-	}
-	tickEmail.style.display = "block";
-	exclamationEmail.style.display = "none";
-	loading.style.display = "none";
-	errorMsgs[0].style.display = "none";
-	nextPage();
-};
+// validation icons/error message on page 3
+tickEmail.style.display = "none";
+exclamationEmail.style.display = "none";
+loading.style.display = "none";
+errorMsgs[0].style.display = "none";
 
-// TODO: these two buttons must connect to the next step of the enrollment form
-submit.onclick = function() {
-	alert("You have submitted the payment!");
-};
-proceedPayment.onclick = function() {
-	alert("Taking you to proceed payment!");
-};
+// hide Submit and Proceed Payment buttons on page 4
+submit.style.display = "none";
+proceedPayment.style.display = "none";
 
-// back buttons onclick function goes to the previous page
-back3.onclick = function() {
-	previousPage();
-};
-back4.onclick = function() {
-	previousPage();
-};
+// ==========================================
+//             Page Navigation
+// ==========================================
 
 // go to the next page of the membership system
 function nextPage() {
@@ -141,47 +109,73 @@ function previousPage() {
 	}
 }
 
-// finds the page that is currently active
-function findActivePage() {
-	if (isActive(page1)) return 1;
-	else if (isActive(page2)) return 2;
-	else if (isActive(page3)) return 3;
-	else if (isActive(page4)) return 4;
-}
+// Enter key (keycode 13) triggers the click event of the appropriate buttons to go to next page.
+window.addEventListener("keypress", function(e) {
+	if (e.keyCode === 13) {
+		switch (findActivePage()) {
+			case 1:
+				buttonReg.click(); //buttonReg does not need an onclick function declaration because it is handled by Webflow
+				break;
+			case 2:
+				ok2.click(); //ok2 does not need an onclick function declaration because it is handled by Webflow
+				break;
+			case 3:
+				ok3.click();
+				break;
+			case 4:
+				if (isActive(submit)) submit.click();
+				else if (isActive(proceedPayment)) proceedPayment.click();
+				break;
+		}
+	}
+});
 
-// Checks if an element is active (i.e. on the page)
-function isActive(el) {
-	return !(el.offsetParent === null);
-}
+// name/email page (page 3) OK button onclick name and email validation
+ok3.onclick = function() {
+	if (inputName.value.length === 0) {
+		console.log("Name has no value", inputName);
+		return;
+	}
+	if (inputEmail.value.length === 0) {
+		console.log("Email has no value", inputEmail);
+		tickEmail.style.display = "none";
+		exclamationEmail.style.display = "block";
+		errorMsgs[0].style.display = "block";
+		return;
+	}
+	if (!validateEmail(inputEmail.value)) {
+		console.log("Email is not valid!", inputEmail);
+		tickEmail.style.display = "none";
+		exclamationEmail.style.display = "block";
+		errorMsgs[0].style.display = "block";
+		return;
+	}
+	tickEmail.style.display = "block";
+	exclamationEmail.style.display = "none";
+	loading.style.display = "none";
+	errorMsgs[0].style.display = "none";
+	nextPage();
+};
 
-// email validation and error message icons initially are invisible
-let tickEmail = document.getElementById("tick-email");
-let exclamationEmail = document.getElementById("exclamation-email");
-let loading = document.getElementById("loading");
-let errorMsgs = document.getElementsByClassName("div-errormsg");
-console.log(errorMsgs);
-tickEmail.style.display = "none";
-exclamationEmail.style.display = "none";
-loading.style.display = "none";
-errorMsgs[0].style.display = "none";
+// TODO: these two buttons must connect to the next step of the enrollment form
+submit.onclick = function() {
+	alert("You have submitted the payment!");
+};
+proceedPayment.onclick = function() {
+	alert("Taking you to proceed payment!");
+};
 
-// validates the email by checking if the input is in the right format (e.g. johnsmith@example.com)
-function validateEmail(email) {
-	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
-}
+// back buttons onclick function goes to the previous page
+back3.onclick = function() {
+	previousPage();
+};
+back4.onclick = function() {
+	previousPage();
+};
 
-// get the button elements
-let payCash = document.getElementById("btn-cash");
-let payTransfer = document.getElementById("btn-banktransfer");
-let payWeChat = document.getElementById("btn-wechat");
-let payAli = document.getElementById("btn-alipay");
-let payCard = document.getElementById("btn-creditcard");
-let payPoli = document.getElementById("btn-poliay");
-
-// initially hide Submit and Proceed Payment buttons
-submit.style.display = "none";
-proceedPayment.style.display = "none";
+// ==========================================
+//    Payment Page (page 4) Functionality
+// ==========================================
 
 // sets up event listener for button click
 [payCash, payTransfer, payWeChat, payAli, payCard, payPoli].forEach(
@@ -213,4 +207,27 @@ function showButton(index) {
 		submit.style.display = "none";
 		proceedPayment.style.display = "flex";
 	}
+}
+
+// ==========================================
+//             Helper Functions
+// ==========================================
+
+// finds the page that is currently active
+function findActivePage() {
+	if (isActive(page1)) return 1;
+	else if (isActive(page2)) return 2;
+	else if (isActive(page3)) return 3;
+	else if (isActive(page4)) return 4;
+}
+
+// Checks if an element is active (i.e. on the page)
+function isActive(el) {
+	return !(el.offsetParent === null);
+}
+
+// validates the email by checking if the input is in the right format (e.g. johnsmith@example.com)
+function validateEmail(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
