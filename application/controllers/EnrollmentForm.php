cb450ec7c59f7ec74df16b3497dc3b8a3097b7d9
Raymond Feng <raymondf.5522@gmail.com>
2020-03-23 22:20:59 +1300

    [ASPA-008] Recording-User-Information (#6)
    * [ASPA-008] Gsheet API Interface functions rearranging (copy-paste from 007 for git branch purposes)
    
    ISSUE: Needed some utility functions
    SOLUTION:
      - Allowed different spreadsheets to be used given a Gsheet_Interface_Model class creation
      - (New Function) Set Row highlighting based on any cell, based on given [R, G, B] code – to change
      - (New Function) Get cell colour, returns hex code
      - (New Function) Get cell 'range' (i.e A1) based on a string from a column
    RISK: When 007 merges with master, potential for lots of conflicts needing to be resolved (sorry Jiaru!)
    REVIEWED BY: Lucas
    
    * [ASPA-008] Added 'SHEETNAME' constant to constants.php to fix error
    Risk: -
    Reviewed by: Lucas
    
    * [ASPA-008] Added "split_column_row" function
    Issue: Needed a way to safely get the individual row and column values
    Solution: Added such a function, returning input: 'AA43' into [(str) 'AA', (int) 43]
    Risk: None
    Reviewed by: Lucas
    
    * [ASPA-008] Decluttered EnrollmentForm testing function from examples

--- application/controllers/EnrollmentForm.php
+++ application/controllers/EnrollmentForm.php
@@ -18,26 +18,7 @@ class EnrollmentForm extends CI_Controller {
 	{
         $this->load->model('Gsheet_Interface_Model');
 
-
-        // Records a user to spreadsheet
-        // $this->Gsheet_Interface_Model->record_to_sheet('testemail@gmail.com', 'Test Person', 'CASH', TRUE);
-
-
-        // Example read request
-        $values = $this->Gsheet_Interface_Model->get_from_sheet('B1', 'B30');
-
-        foreach ($values as $row) {
-            if (empty($row)) {
-
-            } else {
-                foreach ($row as $value) {
-                    echo $value . "<br />";
-                }
-            }
-        }
-
-        echo $this->Gsheet_Interface_Model->get_sheet_size();
-
+        // Test out functions here
 
 	}
 }
