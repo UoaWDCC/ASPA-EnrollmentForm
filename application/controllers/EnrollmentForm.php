ce5e81184acdcfa5746f57223ec48423c8e927e4
Jiaru <lin8231@outlook.com>
2020-03-30 20:37:08 +1300

    [ASPA-007] Integration of Email Validation
    Issue: validite() function needed updating to differentiate between error messages
    Solution: updated validate() function to give 3 types of responses (success, signed up but not paid, not signed up)
    Risk: fields of create json functio may need to be altered or removed
    Reviewed by: Martin

--- application/controllers/EnrollmentForm.php
+++ application/controllers/EnrollmentForm.php
@@ -34,12 +34,15 @@ class EnrollmentForm extends ASPA_Controller
 	 */
 	public function validate() {
 		$emailAddress = $this->input->post('emailAddress');
-		$this->load->model('Verification_Model');
-		$result = $this->Verification_Model->is_email_on_sheet($emailAddress);
-		if ($result) {
-			$this->create_json('True', '', 'On sheet');
+		$this->load->model('Verification_Model', 'verificationModel');
+		if ($this->verificationModel->has_user_paid($emailAddress)) {
+			$this->create_json('True', '', 'Success');
 		} else {
-			$this->create_json('False', '', 'Not on sheet');
+			if ($this->verificationModel->is_email_on_sheet($emailAddress)){
+				$this->create_json('False', '', 'Error: signed up but not paid');
+			} else {
+				$this->create_json('False', '', 'Error: not signed up');
+			}
 		}
 	}
 
