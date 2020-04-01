f8ea7c5756ced7c3034b730ff93269e6b06314ac
Will-ZYS <willyzhysh@gmail.com>
2020-04-01 23:35:45 +1300

    removed welcome controller

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'localhost:8888/ASPA-EnrollmentForm/index.php/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
