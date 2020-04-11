f92b992055819b4536153db5853452c6ad1dece2
Raymond Feng <raymondf.5522@gmail.com>
2020-04-11 14:09:45 +1200

    [ASPA-017] Fixing path bug for server deployment. Issue: as above. Solution: Made into relative path instead. Risk area: - Reviewed by: Lucas

--- application/views/PaymentSuccessful.php
+++ application/views/PaymentSuccessful.php
@@ -14,14 +14,14 @@
         <meta name='viewport' content='initial-scale=1.0' />
     	<meta name="author" content="UoA Web Development & Consulting Club members">
 
-        <link type='text/css' rel='stylesheet' href='http://localhost/assets/css/ConfirmationStyle.css' />
+        <link type='text/css' rel='stylesheet' href='assets/css/ConfirmationStyle.css' />
     </head>
 
     <body>
         <div class="centre-page">
 
             <br />
-            <image id="green-tick" src="http://localhost/assets/images/Green-Confirmation-Tick.jfif"<?php echo $has_paid ? "" : "style='filter: grayscale(1); -webkit-filter: grayscale(1)'"; ?> />
+            <image id="green-tick" src="assets/images/Green-Confirmation-Tick.jfif"<?php echo $has_paid ? "" : "style='filter: grayscale(1); -webkit-filter: grayscale(1)'"; ?> />
 
             <h1>
                 <?php echo $has_paid ? "Paid!" : "Submitted!"; ?>
