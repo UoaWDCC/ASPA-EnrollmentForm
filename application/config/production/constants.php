b9a8c1d5ae899b9b4a7159663f7d9de6c6109961
Lucas Gao <lucas2005gao@gmail.com>
2020-05-30 22:24:05 +1200

    Defined new constants for production environment, and added auto environment detection

---
+++ application/config/production/constants.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Custom Defined Constants for Deployment Environment
+|--------------------------------------------------------------------------
+| Note that constants in this file is loaded first, then the constants in the main folder
+| https://codeinphp.github.io/post/development-environments-in-codeigniter/
+*/
+define('SPREADSHEETID','1NJ3gFsf1qP_-5NF2XyistqUzkug99S656I30oa8-iLU');
+define("SHEETNAME", "Catch");
+
+
+define('PUBLICKEY', 'pk_live_oJK5I9uVYV1gtOEvazhNdaL9000iRzyq76');
+define('SECRETKEY', 'sk_live_EUApQsczjgsIqDThBRN4G4JP00lwLN00R6');
+
+
+define("MEMBERSHIP_SPREADSHEETID", '10mwPhiOR_Vfsfw8WHereu4Y5KOsuSWkJFGhrf6Mfk9I');
+define("MEMBERSHIP_SHEETNAME", 'Sheet1');
