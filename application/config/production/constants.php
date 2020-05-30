c3e62551665c6d0d5a83629ddd74049e77eba856
Lucas Gao <lucas2005gao@gmail.com>
2020-05-30 23:40:39 +1200

    Stripe credentials retrival functionality completed (+6 squashed commit)
    Squashed commit:
    
    [a9b4ca8] fixed typo
    
    [f470937] TestConstants updated
    
    [95bb108] debugging
    
    [eec878b] Changed publickey variable name
    
    [3de6517] Fixed path name
    
    [2820b18] testing stripe constants

--- application/config/production/constants.php
+++ application/config/production/constants.php
@@ -11,10 +11,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 define('SPREADSHEETID','1NJ3gFsf1qP_-5NF2XyistqUzkug99S656I30oa8-iLU');
 define("SHEETNAME", "Catch");
 
-
-define('PUBLICKEY', 'pk_live_oJK5I9uVYV1gtOEvazhNdaL9000iRzyq76');
-define('SECRETKEY', 'sk_live_EUApQsczjgsIqDThBRN4G4JP00lwLN00R6');
-
+// setting the stripe publickey and secretkey
+// Get the path of the stripeCredentials.json file
+$stripeCredentials = trim(shell_exec('pwd'))."/stripeCredentials.json";
+// Only assign to live keys if this file exists. If rolls back to test keys.
+if (file_exists($stripeCredentials)) {
+	$json = file_get_contents($stripeCredentials);
+	// Only assign if the json decoding is carried out correctly
+	if ($stripeCredentials = json_decode($json, true)) {
+		if (isset($stripeCredentials["public_key"]) && isset($stripeCredentials["secret_key"])){
+			define('PUBLICKEY', $stripeCredentials["public_key"]);
+			define('SECRETKEY', $stripeCredentials["secret_key"]);
+		}
+	}
+}
 
 define("MEMBERSHIP_SPREADSHEETID", '10mwPhiOR_Vfsfw8WHereu4Y5KOsuSWkJFGhrf6Mfk9I');
 define("MEMBERSHIP_SHEETNAME", 'Sheet1');
