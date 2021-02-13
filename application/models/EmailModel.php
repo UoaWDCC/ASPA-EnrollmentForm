1c5bfa86f8c24cd18370a59571f468dcc9a76ba4
Lucas Gao <lucas2005gao@gmail.com>
2021-02-13 19:17:13 +1300

    added shellexec

--- application/models/EmailModel.php
+++ application/models/EmailModel.php
@@ -279,35 +279,40 @@ class EmailModel extends CI_Model {
         $mail = new PHPMailer(true);
 
         try {
-            //Server settings
-            //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-            $mail->isSMTP();                                            // Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-            $mail->Username   = 'uoawdcc@gmail.com';                     // SMTP username
-            $mail->Password   = 'orkyhxoabrnpqeri';                               // SMTP password
-            // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-            //Recipients
-            $mail->setFrom('uoawdcc@gmail.com', 'WDCC');
-            $mail->addAddress($EMAIL_RECIEVER);     // Add a recipient
-            // $mail->addAddress('ellen@example.com');               // Name is optional
-            // $mail->addReplyTo('info@example.com', 'Information');
-            // $mail->addCC('cc@example.com');
-            // $mail->addBCC('bcc@example.com');
+            // //Server settings
+            // //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+            // $mail->isSMTP();                                            // Send using SMTP
+            // $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+            // $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+            // $mail->Username   = 'uoawdcc@gmail.com';                     // SMTP username
+            // $mail->Password   = 'orkyhxoabrnpqeri';                               // SMTP password
+            // // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+            // $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-            // Attachments
-            // $mail->AddEmbeddedImage('assets/images/ASPA_logo.png','ASPA_logo');
-            // $mail->AddEmbeddedImage($TICK_IMAGE,'Tick');
-            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+            // //Recipients
+            // $mail->setFrom('uoawdcc@gmail.com', 'WDCC');
+            // $mail->addAddress($EMAIL_RECIEVER);     // Add a recipient
+            // // $mail->addAddress('ellen@example.com');               // Name is optional
+            // // $mail->addReplyTo('info@example.com', 'Information');
+            // // $mail->addCC('cc@example.com');
+            // // $mail->addBCC('bcc@example.com');
 
-            // Content
-            $mail->isHTML(true);                                  // Set email format to HTML
-            $mail->Subject = $EMAIL_SUBJECT;
-            $mail->Body    = $message;
-            $mail->AltBody = 'Thank you for signing up to ' . $eventData["title"] . 'This text is shown due to your device\'s restriction on email content. Please contact ' . $EMAIL_SENDER . ' for clarification.';
+            // // Attachments
+            // // $mail->AddEmbeddedImage('assets/images/ASPA_logo.png','ASPA_logo');
+            // // $mail->AddEmbeddedImage($TICK_IMAGE,'Tick');
+            // // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            // // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            // // Content
+            // $mail->isHTML(true);                                  // Set email format to HTML
+            // $mail->Subject = $EMAIL_SUBJECT;
+            // $mail->Body    = $message;
+            // $mail->AltBody = 'Thank you for signing up to ' . $eventData["title"] . 'This text is shown due to your device\'s restriction on email content. Please contact ' . $EMAIL_SENDER . ' for clarification.';
+
+            log_message('info', "=========sending email=============");
+            log_message('info', shell_exec("echo '".$message."'| mail -v -s '".$EMAIL_SUBJECT."' ".$EMAIL_RECIEVER));
+
 
             $mail->send();
         } catch (Exception $e) {
