0423b07ac76bdc71c18efb5869bdcd9e67082e10
Lucas Gao <lucas2005gao@gmail.com>
2021-02-14 00:38:44 +1300

    email template header

--- application/models/EmailModel.php
+++ application/models/EmailModel.php
@@ -309,12 +309,11 @@ class EmailModel extends CI_Model {
             // $mail->Subject = $EMAIL_SUBJECT;
             // $mail->Body    = $message;
             // $mail->AltBody = 'Thank you for signing up to ' . $eventData["title"] . 'This text is shown due to your device\'s restriction on email content. Please contact ' . $EMAIL_SENDER . ' for clarification.';
-
             log_message('info', "=========sending email==============");
-            log_message('info', shell_exec("echo '".$message."' > email.html"));
             log_message('info', shell_exec("pwd"));
-            log_message('info', shell_exec("mailx -a 'Content-Type: text/html' -v -s '".$EMAIL_SUBJECT."' ".$EMAIL_RECIEVER." < email.html"));
-
+            $EMAIL_SUBJECT = "$(echo -e '".$EMAIL_SUBJECT."\nContent-Type: text/html')" ;
+            log_message('info', shell_exec("echo ".$message."| mailx -a 'Content-Type: text/html' -v -s ".$EMAIL_SUBJECT." ".$EMAIL_RECIEVER));
+            
             // $mail->send();
         } catch (Exception $e) {
         }
