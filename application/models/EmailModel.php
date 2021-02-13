46946574c5b95a20558266874be6d16d262c908d
Lucas Gao <lucas2005gao@gmail.com>
2021-02-14 00:20:01 +1300

    added quote

--- application/models/EmailModel.php
+++ application/models/EmailModel.php
@@ -311,7 +311,7 @@ class EmailModel extends CI_Model {
             // $mail->AltBody = 'Thank you for signing up to ' . $eventData["title"] . 'This text is shown due to your device\'s restriction on email content. Please contact ' . $EMAIL_SENDER . ' for clarification.';
 
             log_message('info', "=========sending email==============");
-            log_message('info', shell_exec("echo ".$message." > email.html"));
+            log_message('info', shell_exec("echo '".$message."' > email.html"));
             log_message('info', shell_exec("mailx -a 'Content-Type: text/html' -v -s '".$EMAIL_SUBJECT."' ".$EMAIL_RECIEVER." < email.html"));
 
             // $mail->send();
