62d4956dfeccd1a8d348711e6fb05b96e3dfb04d
Lucas Gao <lucas2005gao@gmail.com>
2021-02-13 17:09:46 +1300

    added instructions to readme and added to start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+echo "======> Copying sample_database.php to database.php"
 cp ./application/config/sample_database.php ./application/config/database.php 
+echo "DONE"
+echo "\n"
+
+echo "======> Going into the php docker image"
+docker exec -it php-httpd /bin/bash
+echo "======> Running composer install"
+composer install
+echo "DONE"
+echo "\n"
