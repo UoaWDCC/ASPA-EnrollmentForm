62dbf0322a51562f789948153e78595d3780feb3
Raymond Feng <rf.raymondfeng@gmail.com>
2021-02-15 22:16:39 +1300

    added start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "======> Copying sample_database.php to database.php"
-cp ./application/config/sample_database.php ./application/config/database.php 
+cp ./application/config/sample_database.php ./application/config/database.php
 echo "DONE"
 read -p 'Press Enter to continue...' var
 
