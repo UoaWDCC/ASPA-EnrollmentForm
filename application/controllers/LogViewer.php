a0b4e11a4aba1e6060715cd6784def8c1ed3c543
Lucas Gao <lucas2005gao@gmail.com>
2021-03-13 22:51:55 +1300

    added IP whitelisting

--- application/controllers/LogViewer.php
+++ application/controllers/LogViewer.php
@@ -10,10 +10,14 @@ class LogViewer extends CI_Controller
     public function __construct() {
         parent::__construct(); 
         $this->logViewer = new \CILogViewer\CILogViewer();
-        //...
+        log_message('debug', "=====New Log Controller Initialized====");
     }
 
     public function index() {
+        $whitelist = array('127.0.0.1', '::1');
+        if (!in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
+            show_404('Log page with IP:'.$_SERVER['REMOTE_ADDR'],TRUE);
+        } 
         echo $this->logViewer->showLogs();
         return;
     }
