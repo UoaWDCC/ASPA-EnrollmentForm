405b6dbbcd68ad01f13521d915ba86472557b4d4
lucas2005gao <48196609+lucas2005gao@users.noreply.github.com>
2021-03-14 22:44:38 +1300

    [ASPA-016] Add James' IP for IP Whitelisting (#64)
    Issue:
    James' computer uses IPv6 so he doesn't have access to the LogViewers
    
    Solution:
    Added James' IPv6 address
    
    Risk:
    None
    
    Reviewed By:
    James

--- application/controllers/LogViewer.php
+++ application/controllers/LogViewer.php
@@ -14,7 +14,7 @@ class LogViewer extends CI_Controller
     }
 
     public function index() {
-        $whitelist = array('127.0.0.1', '::1', '118.92.25.130', '101.98.193.212');
+        $whitelist = array('127.0.0.1', '::1', '118.92.25.130', '101.98.193.212', '2404:4408:673a:9400:a9ec:66a6:1e6b:a809');
 
         // Determine my IP Address
         if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
