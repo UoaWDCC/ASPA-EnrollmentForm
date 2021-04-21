2092fd884a576ba942bdf1ca9b937231ca958a4c
Raymond Feng <rf.raymondfeng@gmail.com>
2021-04-22 10:03:13 +1200

    [ASPA-4] Add "mail.email" and "mail.password" fields to new auth_props.json file

--- application/config/constants.php
+++ application/config/constants.php
@@ -99,8 +99,18 @@ const REGISTRATION_SPREADSHEET_ID = '1YvLmK7DbVuf5O3UDKlYaNOH1Zd8ohg_eEibzwfh2IM
 /**
  * Stripe public and private keys for development.
  */
-define('STRIPE_PUBLIC_KEY', 'pk_test_A4wjqVPPn530rgAXv6sHKgSl00opCMVX9A');
-define('STRIPE_PRIVATE_KEY', 'sk_test_OMC00A11yJUakUU4kx6KoGTp0028EYnLBa');
+if (!isset($STRIPE_PUBLIC_KEY)) {
+    define('STRIPE_PUBLIC_KEY', 'pk_test_A4wjqVPPn530rgAXv6sHKgSl00opCMVX9A');
+    define('STRIPE_PRIVATE_KEY', 'sk_test_OMC00A11yJUakUU4kx6KoGTp0028EYnLBa');
+}
+
+if (!isset($MAIL_AUTH_EMAIL)) {
+    $json = file_get_contents("private_keys/auth_props.json");
+    $authProps = json_decode($json, true);
+
+    define('MAIL_AUTH_EMAIL', $authProps["mail.email"]);
+    define('MAIL_AUTH_PASSWORD', $authProps["mail.password"]);
+}
 
 /**
  * Google sheet ID and sheet name for ASPA's membership spreadsheet.
