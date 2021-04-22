272a60c4bc8deafb3283e8bcf874414cc2cfb0d2
Raymond Feng <rf.raymondfeng@gmail.com>
2021-04-23 09:30:16 +1200

    [ASPA-4] Make changes for @lucas2005gao's PR

--- application/config/constants.php
+++ application/config/constants.php
@@ -99,12 +99,12 @@ const REGISTRATION_SPREADSHEET_ID = '1YvLmK7DbVuf5O3UDKlYaNOH1Zd8ohg_eEibzwfh2IM
 /**
  * Stripe public and private keys for development.
  */
-if (!isset($STRIPE_PUBLIC_KEY)) {
+if (!defined('STRIPE_PUBLIC_KEY')) {
     define('STRIPE_PUBLIC_KEY', 'pk_test_A4wjqVPPn530rgAXv6sHKgSl00opCMVX9A');
     define('STRIPE_PRIVATE_KEY', 'sk_test_OMC00A11yJUakUU4kx6KoGTp0028EYnLBa');
 }
 
-if (!isset($MAIL_AUTH_EMAIL)) {
+if (!defined('MAIL_AUTH_EMAIL')) {
     $json = file_get_contents("private_keys/auth_props.json");
     $authProps = json_decode($json, true);
 
