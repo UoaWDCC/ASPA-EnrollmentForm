c3466cf6e89b30e62561f07912132013997957be
Raymond Feng <raymondf.5522@gmail.com>
2021-04-26 20:24:11 +1200

    [ASPA-13] Setup Admin.php controller/route
    Issue:
    There needs to be a file created so that @Ama911 and @Metarock can both work on their assigned tasks of ASPA-14 and ASPA-31 together.
    
    Solution:
    Created this file with the template endpoints.
    
    Risk:
    n/a
    
    Reviewed By:
    @jameszu and @Metarock

---
+++ application/controllers/Admin.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Handles all admin-checkup app related endpoints and views.
+ */
+class Admin extends ASPA_Controller
+{
+
+    public function markAsPaid() {
+        // TODO: ASPA-31
+    }
+
+    public function paymentStatus() {
+        // TODO: ASPA-14
+    }
+
+}
