e724891d3909c42bdc4b4f3193ee87279f3ea6da
Raymond Feng <rf.raymondfeng@gmail.com>
2021-04-26 20:23:21 +1200

    [ASPA-13] Add commenting changes

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,6 +1,9 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-
+/**
+ * Handles all admin-checkup app related endpoints and views.
+ */
 class Admin extends ASPA_Controller
 {
 
