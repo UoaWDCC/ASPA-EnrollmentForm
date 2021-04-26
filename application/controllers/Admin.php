c14dc1779076970766fa90990cb3123e6063cd37
Raymond Feng <rf.raymondfeng@gmail.com>
2021-04-26 20:19:45 +1200

    [ASPA-13] Setup Admin.php controller/route

---
+++ application/controllers/Admin.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Admin extends ASPA_Controller
+{
+
+    public function markAsPaid() {
+        // TODO: ASPA-31
+    }
+
+    public function paymentStatus() {
+        // TODO: ASPA-14
+    }
+
+}
