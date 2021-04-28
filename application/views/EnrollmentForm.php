d0f61ea854c6d646f7834b8e1f0f3ef028d6fb84
VictorGan2 <69078157+VictorGan2@users.noreply.github.com>
2021-04-29 08:11:44 +1200

    [ASPA-17] change element location
    the link has been put at the bottom of the div-name-email class

--- application/views/EnrollmentForm.php
+++ application/views/EnrollmentForm.php
@@ -76,15 +76,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <p class="p p-errormessage">Unrecognized email, please use the email you signed up to ASPA with.<br></p>
                 <p class="p p-errormessage">If you are not a member yet, please register first.</p>
               </div>
+              <div>
+                <p><a class="signuplink" href="https://docs.google.com/forms/d/e/1FAIpQLSezUAfbTZKBV-J0yJleBn3RkvJj0xHIinqLsc5aqKCdxgzZSw/viewform">Sign up to ASPA</a> if you're not a member</p>
+              </div>
             </div>
             <div id="div-ok3" class="div-okbtn">
               <div class="div-placeholder ok-btn-spaceholder"></div><a id="ok3" data-w-id="ca7b1509-24a9-effc-9e1e-24dfb568363f" href="#" class="button btn-ok w-button">OK ✓</a>
               <p class="p-ok-btn-confirm p">Press<strong> Enter ↵</strong></p>
             </div>
             <div id="div-back-page3" class="div-back div-page-page3"><a id="btn-back-page3" href="#" class="btn-back w-button">← Back</a></div>
-              <div>
-                <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSezUAfbTZKBV-J0yJleBn3RkvJj0xHIinqLsc5aqKCdxgzZSw/viewform">Sign up to ASPA</a> if you're not a member</p>
-              </div>
           </div>
           <div id="div-page4" class="div-block page4">
             <div class="div">
