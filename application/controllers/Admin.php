88ee9cc97fc26fd3ad976837cadd5e1bcc7774d3
Metarock <esang037@gmail.com>
2021-05-04 00:12:22 +1200

    fixed cannot modify header information

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -37,6 +37,24 @@ class Admin extends ASPA_Controller
         //ONE OF THEM IS REQUIRED, EITHER.
         $email = $this->input->get('email');
         $upi = $this->input->get('upi');
+<<<<<<< HEAD
+=======
+        //get google sheets
+        // $this->load->library('../controllers/EnrollmentForm.php');
+        // echo $this->EnrollmentForm->loadEventData();
+        $eventData;
+        $data = $this->GoogleSheets_Model->getCellContents('A2', 'C' . ($this->GoogleSheets_Model->getNumberOfRecords() + 2));
+
+        // Important variables we care about
+        $elements = ['time', 'date', 'location', 'title', 'tagline', 'price', 'acc_num', 'desc', 'gsheet_name', 'form_enabled'];
+
+        // If the data from spreadsheet contains event details we are looking for, set them.
+        for ($i = 0; $i < sizeof($data); $i++) {
+            if (in_array($data[$i][0], $elements)) {
+                $this->eventData[$data[$i][0]] = $data[$i][2];
+            }
+        }
+>>>>>>> a11cd04 (fixed cannot modify header information)
         
         $eventData;
 
@@ -63,6 +81,7 @@ class Admin extends ASPA_Controller
         // $isEmail = $this->Verification_Model->isEmailOnSheet($email, REGISTRATION_SPREADSHEET_ID, $this->eventData['gsheet_name']);
         // $isUpi = $this->Verification_Model->isUpiOnSheet($upi, REGISTRATION_SPREADSHEET_ID, $this->eventData['gsheet_name']);
 
+<<<<<<< HEAD
         //if email or upi is not found in the google sheets.
         if(!$isEmail){
             $this->output->set_status_header(404, "error")->_display("Attendee not found");
@@ -75,6 +94,18 @@ class Admin extends ASPA_Controller
         // }
         // else{
         //     $this->output->set_status_header(412)->_display("Queries not specified");
+=======
+        if($email || $upi ){
+            //if email or upi is not found in the google sheets.
+            if(!$isEmail){
+                $this->output->set_status_header(404, "error")->_display("Attendee not found");
+                exit();
+            }
+            $this->output->set_status_header(200)->_display("Successfully, marked attendee as paid");
+        }
+        else{
+            $this->output->set_status_header(412)->_display("Queries not specified");
+>>>>>>> a11cd04 (fixed cannot modify header information)
 
         // }
     }
