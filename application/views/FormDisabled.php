d492a8348a66962447008b5fce352563e16a5bfe
Matthew Ouyang <wouy448@aucklanduni.ac.nz>
2021-05-05 19:28:33 +1200

    [ASPA-10] improve code styling

--- application/views/FormDisabled.php
+++ application/views/FormDisabled.php
@@ -2,40 +2,40 @@
 
 <html>
 
-<head>
-  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
-  <script type="text/javascript">
-    WebFont.load({
-      google: {
-        families: ["Droid Sans:400,700", "PT Serif:400,400italic,700,700italic"]
-      }
-    });
-  </script>
+  <head>
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
+    <script type="text/javascript">
+      WebFont.load({
+        google: {
+          families: ["Droid Sans:400,700", "PT Serif:400,400italic,700,700italic"]
+        }
+      });
+    </script>
 
-  <title>Form disabled • ASPA</title>
-  <meta charset="utf-8">
-  <meta name="description" content="Form Disabled">
-  <meta name='viewport' content='initial-scale=1.0' />
-  <meta name="author" content="UoA Web Development & Consulting Club members">
+    <title>Form disabled • ASPA</title>
 
-  <link href="assets/css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="assets/css/webflow.css" rel="stylesheet" type="text/css">
-  <link href="assets/css/aspa.webflow.css" rel="stylesheet" type="text/css">
-  <link href='assets/css/formDisabled.css' rel='stylesheet' type='text/css' />
-  <link href="assets/images/favicon.png" rel="icon" type="image/png">
-</head>
+    <meta charset="utf-8">
+    <meta name="description" content="Form Disabled">
+    <meta name='viewport' content='initial-scale=1.0' />
+    <meta name="author" content="UoA Web Development & Consulting Club members">
 
-<body>
-  <div class="centre-page">
-    <img src="assets/images/ASPA_logo.png" />
-    <h1>Sorry about that.</h1>
-    <p>Sign up is not open yet. Stay posted to ASPA social media pages to stay up to date on the latest events!</p>
-    <p>Contact
-      <a href="mailto:uoapool@gmail.com">uoapool@gmail.com</a> 
-      if you have any questions.
-    </p>
-  </div>
+    <link href="assets/css/normalize.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/webflow.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/aspa.webflow.css" rel="stylesheet" type="text/css">
+    <link href='assets/css/formDisabled.css' rel='stylesheet' type='text/css' />
+    <link href="assets/images/favicon.png" rel="icon" type="image/png">
+  </head>
 
-</body>
+  <body>
+    <div class="centre-page">
+      <img src="assets/images/ASPA_logo.png" />
+      <h1>Sorry about that.</h1>
+      <p>Sign up is not open yet. Stay posted to ASPA social media pages to stay up to date on the latest events!</p>
+      <p>Contact
+        <a href="mailto:uoapool@gmail.com">uoapool@gmail.com</a>
+        if you have any questions.
+      </p>
+    </div>
+  </body>
 
 </html>
