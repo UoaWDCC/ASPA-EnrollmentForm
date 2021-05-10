b20aa8a7a1b5a78cf3f95809d58a6e3a19b5a97b
Metarock <66818636+Metarock@users.noreply.github.com>
2021-05-10 20:40:53 +1200

    [ASPA-31] Implement an HTTP endpoint that marks an attendee as paid
    Issue:
    Implement an HTTP endpoint that marks an attendee as paid
    
    Solution:
    Added an HTTP endpoint [GET] /Admin/markAsPaid with query parameters.
    
    $email = $this->input->get('email'); $upi = $this->input->get('upi');
    
    Added conditions to return http codes: 200 (user is found and marked as paid), 404 (if user is not found on the registration sheet) and 412 (if both query parameters were not specified)
    
    Risk:
    It may cause conflicts with other developers working with Admin Check-in
    
    Reviewed By:
    @Ray-F

--- application/core/ASPA_Controller.php
+++ application/core/ASPA_Controller.php
@@ -3,14 +3,26 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 // Modified base CI controller 
 // So that all controllers can inherit common controller functions
+
+/**
+ * Class ASPA_Controller
+ *
+ * @property GoogleSheets_Model $GoogleSheets_Model
+ */
 class ASPA_Controller extends CI_Controller
 {
+
+    /**
+     * @var array All the information for this event (retrieved from google sheet).
+     */
+    protected $eventData;
 
     function __construct()
     {
         parent::__construct();
         // $this->load->helper();
 		// $this->load->model();
+        $this->eventData = $this->loadEventData();
     }
 
 	/**
@@ -33,6 +45,39 @@ class ASPA_Controller extends CI_Controller
             ->set_output(json_encode($array, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES))
             ->_display();
         exit;
+    }
+
+    /**
+     * This function handles the loading of event data from google sheets.
+     * 
+     * @return array
+     */
+    private function loadEventData() {
+        $this->load->model("GoogleSheets_Model");
+
+        $eventTemp = [];
+
+        // Get event details from spreadsheet from range A2 to size of spreadsheet
+        $this->GoogleSheets_Model->setCurrentSheetName("CurrentEventDetails");
+        $data = $this->GoogleSheets_Model->getCellContents('A2', 'C' . ($this->GoogleSheets_Model->getNumberOfRecords() + 2));
+
+        // Important variables we care about
+        $elements = ['time', 'date', 'location', 'title', 'tagline', 'price', 'acc_num', 'desc', 'gsheet_name', 'form_enabled'];
+
+        // If the data from spreadsheet contains event details we are looking for, set them.
+        for ($i = 0; $i < sizeof($data); $i++) {
+            if (in_array($data[$i][0], $elements)) {
+                $eventTemp[$data[$i][0]] = $data[$i][2];
+            }
+        }
+
+        if ($eventTemp['gsheet_name']) {
+            $this->GoogleSheets_Model->setCurrentSheetName($eventTemp['gsheet_name']);
+        } else {
+            // disable form if no event sheet is found.
+            $eventTemp["form_enabled"] = False;
+        }
+        return $eventTemp;
     }
 }
 
