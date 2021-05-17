e6a96dc39aeef1fc0d05d07f3004aec5cc8d0183
Joe <62530039+joewuthrich@users.noreply.github.com>
2021-05-17 18:36:29 +1200

    changed console.log to echo

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -59,14 +59,14 @@ class Admin extends ASPA_Controller
      * Checks if input key matches. If it does, a cookie is set.
      */
     public function authenticate() {
+        $key = '';
         $key = $this->input->get('key');
 
         if ($key != 'key') {
-            console.log('key does not match')
             return false;
         }
 
-        console.log('this part works');
+        echo 'this part works';
     }
 
     public function paymentStatus() {
