fe5dce9ada29ad422f44dcd1c4939196676a83ec
zhdr64 <april64chen@gmail.com>
2021-05-17 20:37:17 +1200

    [ASPA-37] Add Admin UI styles

--- application/views/Admin.php
+++ application/views/Admin.php
@@ -5,6 +5,7 @@
 <head>
   <base href="<?php echo base_url(); ?>" />
 
+
   <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js" type="text/javascript"></script>
   <script type="text/javascript">
     WebFont.load({
@@ -17,27 +18,55 @@
   <title>Form disabled • ASPA</title>
 
   <meta charset="utf-8">
-  <meta name="description" content="Form Disabled">
+  <meta name="description" content="Admin Page">
   <meta name='viewport' content='initial-scale=1.0' />
   <meta name="author" content="UoA Web Development & Consulting Club members">
 
-  <link href="assets/css/normalize.css" rel="stylesheet" type="text/css">
-  <link href="assets/css/webflow.css" rel="stylesheet" type="text/css">
-  <link href="assets/css/aspa.webflow.css" rel="stylesheet" type="text/css">
-  <link href='assets/css/formDisabled.css' rel='stylesheet' type='text/css' />
-  <link href="assets/images/favicon.png" rel="icon" type="image/png">
+  <link href="assets/css/admin.css" rel="stylesheet" type="text/css">
+
 </head>
 
 <body>
-  <div class="centre-page">
-    <img src="assets/images/ASPA_logo.png" />
-    <h1>Sorry about that.</h1>
-    <p>Sign up is not open yet. Stay posted to ASPA social media pages to stay up to date on the latest events!</p>
-    <p>Contact
-      <a href="mailto:uoapool@gmail.com">uoapool@gmail.com</a>
-      if you have any questions.
-    </p>
+  <div class="admin-page">
+    <img src="assets/images/ASPA-admin.jpg"/>
+
+    <div class="top">
+      <h2>Sign In</h2>
+    </div>
+    <br>
+
+    <div class="email-button">
+      <button class="button">Email/UPI</button>
+    </div>
+    <br>
+
+
+    <button class="button">QR Code</button>
+    <br>
+    <button class="button">Back</button>
+    
   </div>
+
+  <div class="email-page">
+    <p>UPI:*</p>
+    <input type="text" id="login-upi" name="login-upi">
+    <p>Email:*</p>
+    <input type="text" id="login-email" name="login-email">
+    <br>
+    <button class="button">Log In</button>
+    <br>
+    <button class="button">Back</button> 
+  </div>
+
+
+  <div class="QR-code-page">
+    <p>Scan QR Code</p>
+    <br>
+    <button class="button">Back</button>
+</div>
+
+
+  
 </body>
 
 </html>
