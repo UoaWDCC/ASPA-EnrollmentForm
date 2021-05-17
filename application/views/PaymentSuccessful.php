4bc90855a65f95b19d538bfaa2eb732b877656e7
Raymond Feng <rf.raymondfeng@gmail.com>
2021-05-18 01:40:58 +1200

    [ASPA-42] Fix CSS file formatting

--- application/views/PaymentSuccessful.php
+++ application/views/PaymentSuccessful.php
@@ -14,7 +14,7 @@
     	<meta name="author" content="UoA Web Development & Consulting Club members">
 
         <base href="<?php echo base_url(); ?>" >
-        <link type='text/css' rel='stylesheet' href='assets/css/ConfirmationStyle.css?v=1.6' />
+        <link type='text/css' rel='stylesheet' href='assets/css/ConfirmationStyle.css' />
         <link href="assets/images/favicon.png" rel="icon" type="image/png">
     </head>
 
@@ -40,9 +40,6 @@
             <br />
 
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3192.4051599491463!2d174.76121601515302!3d-36.856719779936945!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6d0d47e6230ebb97%3A0x8b3552d2d249693b!2s9%20City%20Road%2C%20Auckland%20CBD%2C%20Auckland%201010!5e0!3m2!1sen!2snz!4v1585956603835!5m2!1sen!2snz" width="350" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-
-
-
         </div>
 
     </body>
