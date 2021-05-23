92df0d158581c5dfae9710717ff8cd8cd4a82419
Raymond Feng <rf.raymondfeng@gmail.com>
2021-05-23 19:01:19 +1200

    [ASPA-38] Change QR code scanner engine

--- application/views/Admin.php
+++ application/views/Admin.php
@@ -31,7 +31,9 @@
 
   <link href="assets/css/admin.css" rel="stylesheet" type="text/css">
 
-  <script type="text/javascript" src="assets/js/instascan.min.js"></script>
+  <!-- QR Code Scanning Library -->
+  <script type="text/javascript" src="assets/lib/qr-scanner.umd.min.js"></script>
+  <script type="text/javascript" src="assets/lib/qr-scanner-worker.min.js"></script>
 </head>
 
 <body>
@@ -65,8 +67,7 @@
       <div class="flex-filler">
       </div>
       <center>
-        <span><i>Scan event QR code here</i></span>
-        <div class="video-container">
+        <div class="video-container" id="reader">
           <video id="preview" width="100px" height="100px"></video>
         </div>
         <p class="p" id="log"></p>
@@ -97,123 +98,95 @@
   </div>
 
   <script type="text/javascript">
-    let lastEmail = "";
-    // HTML fields
-    const logField = document.getElementById("log");
+    const homePage = document.getElementById("home-page");
+    const emailPage = document.getElementById("email-page");
+    const qrCodePage = document.getElementById("qr-code-page");
+    const messagePage = document.getElementById("message-page");
 
-    // create scanner instance
-    const args = {
-      mirror: false,
-      video: document.getElementById('preview')
-    };
-    window.URL.createObjectURL = (stream) => {
-      args.video.srcObject = stream;
-      return stream;
-    };
-    let scanner = new Instascan.Scanner(args);
+    // 1 - home page, 2 - email page, 3 - qr code page
+    const pages = [homePage, emailPage, qrCodePage, messagePage];
 
-    // listens to detection of a qrcode
-    scanner.addListener('scan', function(content) {
-      if (content) {
-        // incase content not in JSON format
-        try {
-          // JSON in the form {"email":"...","eventName":"..."}
-          const details = JSON.parse(content);
-
-          // If the email hasn't been scanned already, we will check the email
-          if (lastEmail !== details.email) {
-            logField.textContent = "Checking: " + details.email + "";
-            checkUser(null, details.email);
-
-            lastEmail = details.email;
-          }
-
-        } catch (e) {
-          console.error(e);
-          logField.textContent = "QR code not correct";
-        }
+    function switchPage(pageNumber) {
+      for (const page of pages) {
+        page.style.display = 'none';
       }
-    });
 
-    // unlocks access to camera
-    Instascan.Camera.getCameras().then(function(cameras) {
-      if (cameras.length > 0) {
-        scanner.start(cameras[0]);
-      } else {
-        console.error('No cameras found.');
-      }
-    }).catch(function(e) {
-      console.error(e);
-      logField.textContent = e;
-    });
-  </script>
-
-
-  <script type="text/javascript">
-
-  const homePage = document.getElementById("home-page");
-  const emailPage = document.getElementById("email-page");
-  const qrCodePage = document.getElementById("qr-code-page");
-  const messagePage = document.getElementById("message-page");
-
-  // 1 - home page, 2 - email page, 3 - qr code page
-  const pages = [homePage, emailPage, qrCodePage, messagePage];
-
-  function switchPage(pageNumber) {
-    for (const page of pages) {
-      page.style.display = 'none';
+      pages[pageNumber - 1].style.display = 'block';
     }
 
-    pages[pageNumber - 1].style.display = 'block';
-  }
+    switchPage(1);
 
-  switchPage(1);
+    const message1 = document.getElementById("HTTP-200-true"); //Registered and paid
+    const message2 = document.getElementById("HTTP-200-false"); //Registered not paid
+    const message3 = document.getElementById("HTTP-409"); //QR Code/Email Input Duplicate
+    const message4 = document.getElementById("HTTP-404"); //Not registered for eventt
 
-  const message1 = document.getElementById("HTTP-200-true"); //Registered and paid
-  const message2 = document.getElementById("HTTP-200-false"); //Registered not paid
-  const message3 = document.getElementById("HTTP-409"); //QR Code/Email Input Duplicate
-  const message4 = document.getElementById("HTTP-404"); //Not registered for eventt
+    // 1 = HTTP-200-true, 2 = HTTP-200-false, 3 = HTTP-409, 4 = HTTP-404
+    const messages = [message1, message2, message3, message4];
 
-  // 1 = HTTP-200-true, 2 = HTTP-200-false, 3 = HTTP-409, 4 = HTTP-404
-  const messages = [message1, message2, message3, message4];
+    function checkUser(customUpi, customEmail) {
 
-  function checkUser(customUpi, customEmail) {
-  
-    const upi = customUpi ?? document.getElementById('check-upi').value;
-    const email = customEmail ?? document.getElementById('check-email').value;
+      const upi = customUpi ?? document.getElementById('check-upi').value;
+      const email = customEmail ?? document.getElementById('check-email').value;
 
-    console.log(upi, email);
+      console.log(upi, email);
 
-    //check if user has registered/paid using ASPA-14
+      //check if user has registered/paid using ASPA-14
 
-    $.ajax({
-      cache: false,
-      url: document.getElementById("base_url").innerHTML + "index.php/Admin/paymentStatus",
-      method: "GET",
-      data: { 
-        "upi": upi, 
-        "email": email
-      },
-      statusCode: {
-        200: function (data) {
-          console.log("Successfully check user.")
-          console.log(JSON.parse(data).paymentMade);
+      $.ajax({
+        cache: false,
+        url: document.getElementById("base_url").innerHTML + "index.php/Admin/paymentStatus",
+        method: "GET",
+        data: {
+          "upi": upi,
+          "email": email
         },
-        404: function (data) {
-          console.log("Error, user not found!");
-        },
-        409: function (data) {
-          console.log("Error, conflicting user!");
+        statusCode: {
+          200: function (data) {
+            console.log("Successfully check user.")
+            console.log(JSON.parse(data).paymentMade);
+          },
+          404: function (data) {
+            console.log("Error, user not found!");
+          },
+          409: function (data) {
+            console.log("Error, conflicting user!");
+          }
         }
-      }
-	});
+  	});
 
-  }
-  
+    }
+  </script>
+
+  <script type="text/javascript">
+    const logField = document.getElementById("log");
+    let lastEmail = "";
+
+    QrScanner.WORKER_PATH = 'assets/lib/qr-scanner-worker.min.js';
+
+    function checkScanResult(result) {
+      try {
+        const decoded = JSON.parse(result);
+        logField.innerHTML = "Email: " + decoded.email;
+
+        if (lastEmail !== decoded.email) {
+          checkUser(null, decoded.email);
+          lastEmail = decoded.email;
+        }
+      } catch (e) {
+        console.log(e);
+        logField.innerHTML = "QR code not correct";
+      }
+    }
+
+    const videoElem = document.getElementById("preview");
+    const qrScanner = new QrScanner(videoElem, checkScanResult);
+    qrScanner.start();
+
 
   </script>
 
-  
+
 </body>
 
 </html>
