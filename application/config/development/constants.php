88ae94e4cd6ca0c8e74f25262ef3d31ab8fdcca3
James <41566813+jameszu@users.noreply.github.com>
2021-05-24 18:22:07 +1200

    Update constants.php

--- application/config/development/constants.php
+++ application/config/development/constants.php
@@ -22,14 +22,6 @@ if (!defined('STRIPE_PUBLIC_KEY')) {
     define('STRIPE_PRIVATE_KEY', 'sk_test_OMC00A11yJUakUU4kx6KoGTp0028EYnLBa');
 }
 
-if (!defined('MAIL_AUTH_EMAIL')) {
-    $json = file_get_contents("private_keys/auth_props.json");
-    $authProps = json_decode($json, true);
-
-    define('MAIL_AUTH_EMAIL', $authProps["mail.email"]);
-    define('MAIL_AUTH_PASSWORD', $authProps["mail.password"]);
-}
-
 /**
  * Google sheet ID and sheet name for ASPA's membership spreadsheet.
  */
