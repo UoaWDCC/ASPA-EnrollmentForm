77d669fc2f213f77bcee980c4d37ba87694bc466
Metarock <esang037@gmail.com>
2021-08-13 22:30:51 +1200

    tseting if this works

---
+++ application/models/entities/Organisation_Model.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Organisation_Model
+{
+
+    public String $name;
+    public String $bankAccountNumber;
+    public String $id;
+    public String $bankRefFormat;
+    public String $logoImg;
+    public String $tagline;
+    public String $supportEmail;
+
+    /**
+     * Constructor for organisation model
+     *
+     * @param String $name
+     * @param String $bankAccountNumber
+     * @param String $id
+     * @param String $bankRefFormat
+     * @param String $logoImg
+     * @param String $tagline
+     * @param String $supportEmail
+     */
+    function __construct($name, $bankAccountNumber, $id, $bankRefFormat, $logoImg, $tagline, $supportEmail)
+    {
+        $this->$name = $name;
+        $this->bankAccountNumber = $bankAccountNumber;
+        $this->id = $id;
+        $this->bankRefFormat = $bankRefFormat;
+        $this->logoImg = $logoImg;
+        $this->tagline = $tagline;
+        $this->supportEmail = $supportEmail;
+    }
+}
