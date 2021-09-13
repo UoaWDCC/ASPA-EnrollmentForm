ab974ee6718e6f03d5e2991c7aba8d2b7c0c44a5
Joe Wuthrich <62530039+joewuthrich@users.noreply.github.com>
2021-09-13 17:53:33 +1200

    [ASPA-52] Add repository class to manage all persistent data
    Issue:
    Multiple instances of the Google_SheetsModel cannot be created for testing or other purposes.
    
    Solution:
    A repository class was added.
    
    The Google_SheetsModel class was also modified to include a new data write/overwrite function.
    
    Risk:
    
    Reviewed By:
    Ray

--- application/models/entities/Member.php
+++ application/models/entities/Member.php
@@ -8,25 +8,16 @@ class Member
     public string $email;
     public string $fullName;
     public string $upi;
-    public int $signUpDate; //PHP int holds 32 bits. Current epoch time is just under 32 bits I beleive
-    public bool $feePain;
+    public int $signUpDate; // PHP int holds 32 bits. Current epoch time is just under 32 bits I believe
+    public bool $feePaid;
 
-    /**
-     * Member constructor.
-     *
-     * @param String $email
-     * @param String $fullName
-     * @param String $upi
-     * @param int $signUpDate
-     * @param bool $feePaid
-     */
     public function __construct(string $email, string $fullName, string $upi, int $signUpDate, bool $feePaid)
     {
         $this->email = $email;
         $this->fullName = $fullName;
         $this->upi = $upi;
         $this->signUpDate = $signUpDate;
-        $this->feePain = $feePaid;
+        $this->feePaid = $feePaid;
     }
 
 }
